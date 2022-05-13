e3232eb64dac27562f9a38f92436ff51f0389338
Ashwani Agarwal <ashwani@Ashwanis-MacBook-Air.local>
2022-05-13 11:37:04 -0400

    Added pre install script

--- pre_install.sh
+++ pre_install.sh
@@ -3,6 +3,6 @@
 DUMP_LOCATION=/data/tools/lyodrugdatabase/db.dump
 
 mkdir /tmp/lyodrugsdata
-mongod --dbpath /tmp/lyodrugsdata  --bind_ip 127.0.0.1 --logpath /tmp/lyodrugsdata.log --logappend &
+/usr/bin/mongod --dbpath /tmp/lyodrugsdata  --bind_ip 127.0.0.1 --logpath /tmp/lyodrugsdata.log --logappend &
 
-monogrestore --archive < DUMP_LOCATION
+/usr/bin/mongorestore --archive < $DUMP_LOCATION
