3428c76945b69e88ae6ebde4ab59967e4cbdfeb3
ashwani1310 <ashwaniagarwal.1310@gmail.com>
2022-05-13 20:50:20 -0400

    Update pre_install.sh

--- pre_install.sh
+++ pre_install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-
+#!/bin/sh
 DUMP_LOCATION=/data/tools/lyodrugdatabase/db.dump
-
+	
 mkdir /tmp/lyodrugsdata
-/usr/bin/mongod --dbpath /tmp/lyodrugsdata  --bind_ip 127.0.0.1 --logpath /tmp/lyodrugsdata.log --logappend &
 
-/usr/bin/mongorestore --archive < $DUMP_LOCATION
+/usr/bin/mongod --dbpath /tmp/lyodrugsdata  --bind_ip 127.0.0.1 --logpath /tmp/lyodrugsdata.log --logappend &
+	
+/usr/bin/mongorestore --archive=${DUMP_LOCATION}
